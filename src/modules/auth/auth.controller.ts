1025f38dc2ed0da323b93ab8ef53e12e9522841b
divitperiwal <divitperiwal@gmail.com>
2026-05-10 08:24:05 +0530

    feat: wrap controller function under asyncHandler to uniform error handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,22 +2,23 @@ import type { Request, Response } from 'express';
 import { loginSchema, registerSchema } from './auth.validation';
 import { AuthService } from './auth.service';
 import { sendSuccess } from '@/utils/response/response';
+import { asyncHandler } from '@/utils/response/async';
 
 export const AuthController = {
-    login: async (req: Request, res: Response) => {
+    login: asyncHandler(async (req: Request, res: Response) => {
         const { email, password } = loginSchema.parse(req.body);
         const { token, user } = await AuthService.login(email, password);
         sendSuccess(res, 200, "User logged in successfully", { token, user });
-
-    },
-    register: async (req: Request, res: Response) => {
+    }),
+    
+    register: asyncHandler(async (req: Request, res: Response) => {
         const { name, email, password } = registerSchema.parse(req.body);
         const { token, user } = await AuthService.register(name, email, password);
         sendSuccess(res, 201, "User registered successfully", { token, user });
+    }),
 
-    },
-    logout: async (req: Request, res: Response) => {
+    logout: asyncHandler(async (req: Request, res: Response) => {
         await AuthService.logout();
         sendSuccess(res, 200, "User logged out successfully");
-    }
+    })
 }
