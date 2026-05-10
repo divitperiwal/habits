f7a8001a34b0829d2089649a5d3cb0c53764ef9e
divitperiwal <divitperiwal@gmail.com>
2026-05-10 08:17:09 +0530

    fix: add name in users table schema

--- src/database/schema/users.schema.ts
+++ src/database/schema/users.schema.ts
@@ -1,7 +1,8 @@
-import {pgTable, text, timestamp, uuid} from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
     id: uuid('id').primaryKey().defaultRandom(),
+    name: text('name').notNull(),
     email: text('email').notNull().unique(),
     password: text('password').notNull(),
     createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
