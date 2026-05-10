3721cadee3e1ffd4a5c5cde435e9d81603b3e3f7
divitperiwal <divitperiwal@gmail.com>
2026-05-10 07:14:46 +0530

    feat: configure helmet and add health route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import express from 'express';
+import helmet from 'helmet';
+import { sendSuccess } from './utils/response/response';
+import { errorHandler, notFound } from './middlewares/error.middleware';
 
 const app = express();
 
+app.use(helmet())
+app.use(express.json());
 
+app.get('/', (_, res) => {
+    sendSuccess(res, 200, 'Welcome to the Habit Tracker API');
+});
+
+app.get('/health', (_, res) => {
+    sendSuccess(res, 200, "Server is healthy", { timestamp: new Date().toISOString() });
+});
+
+app.use(errorHandler)
+app.use(notFound)
 export default app;
